31f058fcc5b9f37f2ccbe6b115e94d3e94ee367b
savhsergio <sergioandresherrera@gmail.com>
2023-01-04 19:16:04 -0500

    inital setup of the project

---
+++ fong_project/contabilidad/models.py
@@ -0,0 +1,19 @@
+from uuid import UUID
+from django.db import models
+
+# Create your models here.
+class Expense(models.Model):
+    pass
+
+
+
+
+class income(models.Model):
+    pass
+    
+    
+    
+    
+    
+    
+    
